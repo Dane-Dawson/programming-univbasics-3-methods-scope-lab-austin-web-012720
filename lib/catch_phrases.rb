a8c87432affba93986b68947c277a9bcee87b25d
Dane P Dawson <dane.p.dawson@gmail.com>
2020-01-11 04:18:02 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -7,3 +7,12 @@ def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+
+def link
+  danger = "It's dangerous to go alone! Take This."
+  puts danger
+end
+
+def any_phrase(words)
+  puts "To bravely go where no man has gone before."
+end
